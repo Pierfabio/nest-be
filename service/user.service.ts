882c4169d2af4cb3b5fe1fe0b64e07653cd6aeca
Pierfabio <sannapierfabio7@gmail.com>
2022-02-20 23:01:27 +0100

    Update tsconfig + main

--- service/user.service.ts
+++ service/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { CreateUserDTO } from '../dto/create-user-dto';
+import { CreateUserDTO } from '../dto/create.user.dto';
 import { User, UserDocument } from '../schemas/user';
 
 @Injectable()
