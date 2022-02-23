e3965a151627b6e80e416f090a010807487ee2d2
Pierfabio <sannapierfabio7@gmail.com>
2022-02-23 21:36:42 +0100

    authentication with jwt

---
+++ src/auth/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+    secret: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkpvaG4gTWFyc3RvbiIsInN1YiI6MSwiaWF0IjoxNjM5OTY5OTI3LCJleHAiOjE2Mzk5Njk5ODd9.X8jNA14s-zfpOYEwwVzKHOHJ_Q1ULpTsgO3OFuHEfMU',
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -8,6 +8,10 @@ import { User, UserDocument } from './schemas/user';
 export class UserService {
   constructor(@InjectModel(User.name) private readonly model: Model<UserDocument>) {}
 
+  async findOne(username: string): Promise<User> {
+    return await this.model.findById(username).exec();
+  }
+
   async create(createUserDto: CreateUserDTO): Promise<User> {
     return await new this.model({
       ...createUserDto,
